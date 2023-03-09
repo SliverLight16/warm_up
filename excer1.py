c4d7ee8069bd289be634791573b0df7745965fde
artisan.jenkins <artisan.jenkins@hgs.hiddengeniusproject.org>
2023-03-08 21:30:02 -0500

    warm up

---
+++ excer1.py
@@ -0,0 +1,11 @@
+def multiSum(num1, num2):
+    if num1 * num2 <= 1000: # condutional statements
+        return num1 * num2 #num1 and num2 are data types
+    
+    else:
+        return num1+num2
+#the if/else is a boolean 
+    
+
+hope = multiSum(32, 2) #function call, arguments
+print(hope)
